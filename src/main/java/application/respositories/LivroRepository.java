9406ba1fa41919230b7ccd646a4e90336c15d059
nataliabaptista <natalia.mendes.baptista@hotmail.com>
2021-09-28 17:58:40 +0000

    .

--- src/main/java/application/respositories/LivroRepository.java
+++ src/main/java/application/respositories/LivroRepository.java
@@ -1,7 +1,10 @@
 package application.respositories;
 import org.springframework.data.repository.CrudRepository;
+
 import application.models.Livro;
 
-public class LivroRepository extends CrudRepository <Livro, Integer> {
+public interface LivroRepository extends CrudRepository<Livro, Integer> {
+
     
+
 }
